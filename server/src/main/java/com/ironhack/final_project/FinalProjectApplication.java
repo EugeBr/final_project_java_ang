26db343a16d26d3694306990d64db1ade773d5f6
EugeBr <eugebordenave@gmail.com>
2023-06-01 10:12:52 +0200

    modify Varchar number in DB

--- server/src/main/java/com/ironhack/final_project/FinalProjectApplication.java
+++ server/src/main/java/com/ironhack/final_project/FinalProjectApplication.java
@@ -2,12 +2,36 @@ package com.ironhack.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
+
+		try {
+			// Connect to your database
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/coffee_project", "root", "ironhack");
+
+			// Create a statement
+			Statement statement = connection.createStatement();
+
+			// Change the VARCHAR length
+			String modDesc = "ALTER TABLE coffee MODIFY COLUMN description VARCHAR(1000)";
+			String modNotes = "ALTER TABLE coffee MODIFY COLUMN notes VARCHAR(1000)";
+			statement.executeUpdate(modDesc);
+			statement.executeUpdate(modNotes);
+
+			// Close the statement and connection
+			statement.close();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
+
