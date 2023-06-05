9737ee5253dea1569faa55c0334a67d9222d17df
EugeBr <eugebordenave@gmail.com>
2023-06-05 18:23:35 +0200

    add new coffee form created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,7 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -54,7 +55,8 @@ import { CoffeeCardComponent } from './components/coffee-card/coffee-card.compon
     ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/new-coffee-form/new-coffee-form.component.ts
+++ client/src/app/components/new-coffee-form/new-coffee-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormArray, FormBuilder  } from '@angular/forms';
 
 @Component({
   selector: 'app-new-coffee-form',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class NewCoffeeFormComponent {
 
+  registerForm: FormGroup;
+  nameInput: FormControl;
+  categoryInput: FormControl;
+  imageUrl: string;
+  descriptionInput: FormControl;
+  prepTimeInput: FormControl;
+  ingredientsInput: FormArray;
+  instructionsInput: FormArray;
+  newIngredient: string;
+  newIstruction: string;
+  notesInput: FormControl;
+
+  constructor(private fb:FormBuilder) {
+
+    this.nameInput = new FormControl('', Validators.required);
+    this.categoryInput = new FormControl('', Validators.required);
+    this.imageUrl = '';
+    this.descriptionInput = new FormControl('', [Validators.required, Validators.maxLength(300)]);
+    this.prepTimeInput = new FormControl('', Validators.required);
+    this.ingredientsInput = this.fb.array([]);
+    this.instructionsInput = this.fb.array([]);
+    this.newIngredient = '';
+    this.newIstruction = '';
+    this.notesInput = new FormControl('', [Validators.required, Validators.maxLength(150)]);
+
+    this.registerForm = new FormGroup({
+      name: this.nameInput,
+      category: this.categoryInput,
+      description: this.descriptionInput,
+      prepTime: this.prepTimeInput,
+      notes: this.notesInput
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Coffee created...'); 
+    this.registerForm.reset();
+  }
+
+  addIngredient(){
+    this.ingredientsInput.push(this.newIngredient);
+  }
 }
