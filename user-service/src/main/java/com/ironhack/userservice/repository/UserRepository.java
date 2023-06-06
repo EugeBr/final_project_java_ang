6a542ddaf0ece803526d16bcb38428b1cb0176db
EugeBr <eugebordenave@gmail.com>
2023-06-06 19:39:16 +0200

    microservice done

---
+++ user-service/src/main/java/com/ironhack/userservice/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ironhack.userservice.repository;
+
+import com.ironhack.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameAndPassword(String username, String password);
+}
